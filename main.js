45890c2812bd755b2dc15c5048d83ffa0aa3a2b8
SammieKei40 <sammiekei9@gmail.com>
2023-01-02 12:40:50 +0100

    Adjusted the height

--- main.js
+++ main.js
@@ -35,20 +35,20 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.button',{}); 
+sr.reveal('.home__img',{}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__subtitle',{}); 
+sr.reveal('.about__text',{}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__img',{});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 600}); 
