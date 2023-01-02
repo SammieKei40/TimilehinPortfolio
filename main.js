1d82b392947e67b2d050663d7f4ad9a9e20b6465
SammieKei40 <sammiekei9@gmail.com>
2023-01-02 13:02:45 +0100

    Added a link

--- main.js
+++ main.js
@@ -28,9 +28,9 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '30px',
+    distance: '80px',
     duration: 2000,
-    reset: true
+    reset: false
 });
 
 /*SCROLL HOME*/
@@ -41,17 +41,17 @@ sr.reveal('.home__social-icon',{ interval: 200});
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{}); 
-sr.reveal('.about__text',{}); 
+sr.reveal('.about__subtitle',{delay: 200}); 
+sr.reveal('.about__text',{delay: 200}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{}); 
-sr.reveal('.skills__img',{});
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 200});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{}); 
+sr.reveal('.work__img',{interval: 200}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{}); 
+sr.reveal('.contact__input',{interval: 200}); 
